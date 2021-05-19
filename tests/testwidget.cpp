fdb14b1bbd786d50697ec3a3cd64e37f5dbe14bb
slamdd <zhouyohu@163.com>
2021-05-19 16:11:11 +0800

    commit

--- tests/testwidget.cpp
+++ tests/testwidget.cpp
@@ -31,127 +31,151 @@ TestWidget::~TestWidget()
 void TestWidget::paintEvent(QPaintEvent *event)
 {
     QWidget::paintEvent(event);
-    QPainter painter(this);
-    painter.setRenderHint(QPainter::Antialiasing, true);
-    painter.fillRect(QRect(FrameWidth, FrameWidth,
-                           this->width() - 2 * FrameWidth,
-                           this->height() - 2 * FrameWidth),
-                     QBrush(Qt::white));
+    QPainter p(this);
+    p.setRenderHint(QPainter::Antialiasing, true);
 
-    QColor color(100, 100, 100, 30);
-    for (int i = 0; i < FrameWidth; i++)
-    {
-        color.setAlpha(120 - qSqrt(i) * 40);
-        painter.setPen(color);
-#if 0
-        painter.drawRect(SHADOW_WIDTH - i,
-                         SHADOW_WIDTH - i,
-                         device->width() - (SHADOW_WIDTH - i) * 2,
-                         device->height() - (SHADOW_WIDTH - i) * 2);
-#else
-        painter.drawRoundedRect(FrameWidth - i,
-                                FrameWidth - i,
-                                this->width() - (FrameWidth - i) * 2,
-                                this->height() - (FrameWidth - i) * 2,
-                                i,
-                                i);
-#endif
-    }
+    /*--------------------------------------------------
+    *   draw caption background
+    *--------------------------------------------------*/
+    p.save();
+    QRect captionRect{FrameWidth,
+                FrameWidth,
+                width() - 2 * FrameWidth,
+                CaptionHeight};
+    p.fillRect(captionRect, QColor(0xCECECE));
+    p.restore();
+
+    /*--------------------------------------------------
+    *   draw client background
+    *--------------------------------------------------*/
+    p.save();
+    QRect clientRect{FrameWidth,
+                FrameWidth + CaptionHeight,
+                width() - 2 * FrameWidth,
+                height() - 2 * FrameWidth - CaptionHeight};
+    p.fillRect(clientRect,
+                     QBrush(Qt::white));
+    p.restore();
+
+    /*--------------------------------------------------
+    *   draw window shadow
+    *--------------------------------------------------*/
+    drawShadow(&p);
+
+    /*--------------------------------------------------
+    *   draw buttons
+    *--------------------------------------------------*/
+    drawButtons(&p);
 }
 
 void TestWidget::mousePressEvent(QMouseEvent *event)
 {
-    if(event->button() == Qt::LeftButton){
-        if(region_ != TenRegion::Middle){
+    if(event->button() == Qt::LeftButton)
+    {
+        if(region_ != Region::HT_Client
+                && region_ != Region::HT_MinButton
+                && region_ != Region::HT_MaxButton
+                && region_ != Region::HT_CloseButton)
+        {
             mouse_left_btn_pressed_ = true;
-            mouse_left_btn_press_pos_ = QCursor::pos();
+            mouse_left_btn_press_pos_ = event->pos();
+        }else if(region_ == Region::HT_MinButton)
+        {
+            min_button_press_ = true;
+        }else if(region_ == Region::HT_MaxButton)
+        {
+            max_buttton_press_ = true;
+        }else if(region_ == Region::HT_CloseButton){
+            close_button_press_ = true;
         }
-    }
-
-    QRect dragRect{FrameWidth,
-                FrameWidth,
-                width() - 2 * FrameWidth,
-                CaptionHeight};
-    if(!dragRect.contains(event->pos())){
-        return QWidget::mousePressEvent(event);
     }
 }
 
 void TestWidget::mouseMoveEvent(QMouseEvent *event)
 {
     if(!mouse_left_btn_pressed_){
-        judgeRegionChangeCursor(event->pos());
-        return QWidget::mouseMoveEvent(event);
-    }
+        judgeRegion(event->pos());
+    }else{
+        QPoint gloPoint = event->globalPos();
+        QRect rect = this->rect();
+        QPoint tl = mapToGlobal(rect.topLeft());
+        QPoint rb = mapToGlobal(rect.bottomRight());
 
-    QPoint gloPoint = event->globalPos();
-    QRect rect = this->rect();
-    QPoint tl = mapToGlobal(rect.topLeft());
-    QPoint rb = mapToGlobal(rect.bottomRight());
-
-    QRect rMove(tl, rb);
-    switch (region_) {
-    case TenRegion::Left:
-    {
-        if(rb.x() - gloPoint.x() <= this->minimumWidth())
-            rMove.setX(tl.x());
-        else
-            rMove.setX(gloPoint.x());
-    }
-        break;
-    case TenRegion::TopLeft:
-    {
-        if(rb.x() - gloPoint.x() <= this->minimumWidth())
-            rMove.setX(tl.x());
-        else
-            rMove.setX(gloPoint.x());
-        if(rb.y() - gloPoint.y() <= this->minimumHeight())
-            rMove.setY(tl.y());
-        else
+        QRect rMove(tl, rb);
+        switch (region_) {
+        case Region::HT_Left:
+        {
+            if(rb.x() - gloPoint.x() <= this->minimumWidth())
+                rMove.setX(tl.x());
+            else
+                rMove.setX(gloPoint.x());
+            setGeometry(rMove);
+        }
+            break;
+        case Region::HT_TopLeft:
+        {
+            if(rb.x() - gloPoint.x() <= this->minimumWidth())
+                rMove.setX(tl.x());
+            else
+                rMove.setX(gloPoint.x());
+            if(rb.y() - gloPoint.y() <= this->minimumHeight())
+                rMove.setY(tl.y());
+            else
+                rMove.setY(gloPoint.y());
+            setGeometry(rMove);
+        }
+            break;
+        case Region::HT_Top:
+        {
+            if(rb.y() - gloPoint.y() <= this->minimumHeight())
+                rMove.setY(tl.y());
+            else
+                rMove.setY(gloPoint.y());
+            setGeometry(rMove);
+        }
+            break;
+        case Region::HT_Caption:
+        {
+            move(event->pos() - mouse_left_btn_press_pos_ + pos());
+        }
+            break;
+        case Region::HT_TopRight:
+        {
+            rMove.setWidth(gloPoint.x() - tl.x());
             rMove.setY(gloPoint.y());
+            setGeometry(rMove);
+        }
+            break;
+        case Region::HT_Right:
+        {
+            rMove.setWidth(gloPoint.x() - tl.x());
+            setGeometry(rMove);
+        }
+            break;
+        case Region::HT_BottomRight:
+        {
+            rMove.setWidth(gloPoint.x() - tl.x());
+            rMove.setHeight(gloPoint.y() - tl.y());
+            setGeometry(rMove);
+        }
+            break;
+        case Region::HT_Bottom:
+        {
+            rMove.setHeight(gloPoint.y() - tl.y());
+            setGeometry(rMove);
+        }
+            break;
+        case Region::HT_BottomLeft:
+        {
+            rMove.setX(gloPoint.x());
+            rMove.setHeight(gloPoint.y() - tl.y());
+            setGeometry(rMove);
+        }
+            break;
+        default:
+            break;
+        }
     }
-        break;
-    case TenRegion::Top:
-    {
-        if(rb.y() - gloPoint.y() <= this->minimumHeight())
-            rMove.setY(tl.y());
-        else
-            rMove.setY(gloPoint.y());
-    }
-        break;
-    case TenRegion::TopRight:
-    {
-        rMove.setWidth(gloPoint.x() - tl.x());
-        rMove.setY(gloPoint.y());
-    }
-        break;
-    case TenRegion::Right:
-    {
-        rMove.setWidth(gloPoint.x() - tl.x());
-    }
-        break;
-    case TenRegion::BottomRight:
-    {
-        rMove.setWidth(gloPoint.x() - tl.x());
-        rMove.setHeight(gloPoint.y() - tl.y());
-    }
-        break;
-    case TenRegion::Bottom:
-    {
-        rMove.setHeight(gloPoint.y() - tl.y());
-    }
-        break;
-    case TenRegion::BottomLeft:
-    {
-        rMove.setX(gloPoint.x());
-        rMove.setHeight(gloPoint.y() - tl.y());
-    }
-        break;
-    default:
-        break;
-    }
-
-    setGeometry(rMove);
     QWidget::mouseMoveEvent(event);
 }
 
@@ -161,59 +185,299 @@ void TestWidget::mouseReleaseEvent(QMouseEvent *event)
     if(event->button() == Qt::LeftButton){
         mouse_left_btn_pressed_ = false;
         mouse_left_btn_press_pos_ = QPoint();
+
+        if(region_ == Region::HT_MinButton)
+        {
+            if(min_button_press_)
+            {
+                showMinimized();
+                min_button_press_ = false;
+            }
+        }else if(region_ == Region::HT_MaxButton)
+        {
+            if(max_buttton_press_){
+                if(this->windowState() == Qt::WindowNoState){
+                    showMaximized();
+                }else{
+                    if(isMaximized()){
+                        showNormal();
+                    }
+                }
+                max_buttton_press_ = false;
+            }
+        }else if(region_ == Region::HT_CloseButton){
+            if(close_button_press_){
+                close();
+                close_button_press_ = false;
+            }
+        }
     }
 }
 
 void TestWidget::leaveEvent(QEvent *event)
 {
-//    mouse_left_btn_pressed_ = false;
-//    press_pos_ = QPoint();
+    clearButtonHover();
+    setCursor(Qt::ArrowCursor);
     QWidget::leaveEvent(event);
 }
 
-void TestWidget::judgeRegionChangeCursor(const QPoint &posInThis)
+void TestWidget::changeEvent(QEvent *event)
 {
-    if(QRect{0, FrameWidth, FrameWidth, height() - 2 * FrameWidth}.contains(posInThis))
+    if(event->type() == QEvent::WindowStateChange){
+        auto windowState = this->windowState();
+        if(windowState == Qt::WindowMaximized || windowState == Qt::WindowFullScreen){
+            FrameWidth = 0;
+        }else{
+            FrameWidth = 10;
+        }
+    }
+}
+
+void TestWidget::judgeRegion(const QPoint &windowPos)
+{
+    clearButtonHover();
+
+    if(QRect{0, FrameWidth, FrameWidth, height() - 2 * FrameWidth}.contains(windowPos))
     {
-        region_ = TenRegion::Left;
+        region_ = Region::HT_Left;
         setCursor(Qt::SizeHorCursor);
-    }else if(QRect{0, 0, FrameWidth, FrameWidth}.contains(posInThis))
+    }else if(QRect{0, 0, FrameWidth + 4, FrameWidth + 4}.contains(windowPos))
     {
-        region_ = TenRegion::TopLeft;
+        region_ = Region::HT_TopLeft;
         setCursor(Qt::SizeFDiagCursor);
-    }else if(QRect{FrameWidth, 0, width() - 2 * FrameWidth, FrameWidth}.contains(posInThis))
+    }else if(QRect{FrameWidth, 0, width() - 2 * FrameWidth, FrameWidth}.contains(windowPos))
     {
+        region_ = Region::HT_Top;
         setCursor(Qt::SizeVerCursor);
     }
-    else if(QRect{width() - FrameWidth, 0, FrameWidth, FrameWidth}.contains(posInThis))
+    else if(QRect{FrameWidth, FrameWidth, width() - 2 * FrameWidth, CaptionHeight}.contains(windowPos))
     {
-        region_ = TenRegion::TopRight;
+        if(btnRect(CaptionButtons::Button_Mini).contains(windowPos)){
+            min_button_hover_ = true;
+            region_ = Region::HT_MinButton;
+        }else if(btnRect(CaptionButtons::Button_NormalMax).contains(windowPos))
+        {
+            max_button_hover_ = true;
+            region_ = Region::HT_MaxButton;
+        }else if(btnRect(CaptionButtons::Button_Close).contains(windowPos))
+        {
+            close_button_hover_ = true;
+            region_ = Region::HT_CloseButton;
+        }else{
+            region_ = Region::HT_Caption;
+        }
+        update();
+        setCursor(Qt::ArrowCursor);
+    }
+    else if(QRect{width() - FrameWidth, 0, FrameWidth, FrameWidth}.contains(windowPos))
+    {
+        region_ = Region::HT_TopRight;
         setCursor(Qt::SizeBDiagCursor);
     }
-    else if(QRect{width() - FrameWidth, FrameWidth, FrameWidth, height() - 2 * FrameWidth}.contains(posInThis))
+    else if(QRect{width() - FrameWidth, FrameWidth, FrameWidth, height() - 2 * FrameWidth}.contains(windowPos))
     {
-        region_ = TenRegion::Right;
+        region_ = Region::HT_Right;
         setCursor(Qt::SizeHorCursor);
     }
-    else if(QRect{width() - FrameWidth, height() - FrameWidth, FrameWidth, FrameWidth}.contains(posInThis))
+    else if(QRect{width() - FrameWidth, height() - FrameWidth, FrameWidth, FrameWidth}.contains(windowPos))
     {
-        region_ = TenRegion::BottomRight;
+        region_ = Region::HT_BottomRight;
         setCursor(Qt::SizeFDiagCursor);
     }
-    else if(QRect{FrameWidth, height() - FrameWidth, width() - 2 * FrameWidth, FrameWidth}.contains(posInThis))
+    else if(QRect{FrameWidth, height() - FrameWidth, width() - 2 * FrameWidth, FrameWidth}.contains(windowPos))
     {
-        region_ = TenRegion::Bottom;
+        region_ = Region::HT_Bottom;
         setCursor(Qt::SizeVerCursor);
     }
-    else if(QRect{0, height() - FrameWidth, FrameWidth, FrameWidth}.contains(posInThis))
+    else if(QRect{0, height() - FrameWidth, FrameWidth, FrameWidth}.contains(windowPos))
     {
-        region_ = TenRegion::BottomLeft;
+        region_ = Region::HT_BottomLeft;
         setCursor(Qt::SizeBDiagCursor);
     }
     else
     {
-        region_ = TenRegion::Middle;
+        region_ = Region::HT_Client;
         setCursor(Qt::ArrowCursor);
     }
+}
+
+QRectF TestWidget::btnRect(TestWidget::CaptionButtons button)
+{
+    int BtnWidth = 45;
+    int BtnHeight = 28;
+    int BtnSpacing = 1;
+
+//    int dpi = this->window()->logicalDpiX();
+//    switch (dpi) {
+//    case 96:
+//        BtnWidth = 45;
+//        BtnHeight = 30;
+//        break;
+//    case 120:
+//        BtnWidth = 56;
+//        BtnHeight = 40;
+//        break;
+//    case 144:
+//        BtnWidth = 68;
+//        BtnHeight = 42;
+//        break;
+//    case 168:
+//        BtnWidth = 78;
+//        BtnHeight = 52;
+//        break;
+//    default:
+//        BtnWidth = 45;
+//        BtnHeight = 28;
+//        break;
+//    }
+
+    QRect captionRect{FrameWidth, FrameWidth, width() - 2 * FrameWidth, CaptionHeight};
+    int y = captionRect.y();
+    QRectF rectClose(captionRect.x() + captionRect.width() - BtnWidth,
+                     y,
+                     BtnWidth,
+                     BtnHeight);
+    QRectF rectMax(rectClose.x() - BtnSpacing - BtnWidth,
+                   y,
+                   BtnWidth,
+                   BtnHeight);
+    QRectF rectMini(rectMax.x() - BtnSpacing - BtnWidth,
+                    y,
+                    BtnWidth,
+                    BtnHeight);
+
+    switch (button) {
+    case CaptionButtons::Button_Close:
+        return rectClose;
+        case CaptionButtons::Button_NormalMax:
+        return rectMax;
+    case CaptionButtons::Button_Mini:
+        return rectMini;
+    default:
+        break;
+    }
+    return QRect();
+
+}
+
+QPixmap TestWidget::btnPixmap(TestWidget::CaptionButtons button)
+{
+    QColor color(Qt::black);
+    QSize size(10, 10);
+    QPixmap pix(size);
+    pix.fill(Qt::transparent);
+    QSizeF sizeF(size.width() *1.0,  size.height() * 1.0);
+    QPainter p(&pix);
+    switch (button) {
+    case CaptionButtons::Button_Mini:
+    {
+        p.save();
+        QPen pen(QBrush(color), 1.0);
+        p.setPen(pen);
+
+        p.drawLine(QPointF(0, sizeF.height() / 2),
+                   QPointF(sizeF.width(), sizeF.height() / 2));
+
+        p.restore();
+    }
+        break;
+    case CaptionButtons::Button_NormalMax:
+    {
+        // max
+        p.save();
+        QPen pen(QBrush(color), 1.0);
+        p.setPen(pen);
+
+        p.drawRect(QRectF(0.0, 0.0, sizeF.width() - 1.0, sizeF.height() - 1.0));
+
+        p.restore();
+    }
+        break;
+    case CaptionButtons::Button_Close:
+    {
+        p.save();
+        p.setRenderHint(QPainter::Antialiasing);
+        QPen pen(QBrush(color), 1.0);
+        p.setPen(pen);
+
+        QRectF rect(0,0,size.width(), size.height());
+        p.drawLine(QPointF(rect.x(), rect.y()),
+                   QPointF(rect.x() + rect.width(), rect.y() + rect.height())
+                   );
+        p.drawLine(QPointF(rect.x() + rect.width(), rect.y()),
+                   QPointF(rect.x(), rect.y() + rect.height())
+                   );
+
+        p.restore();
+    }
+        break;
+    default:
+        break;
+    }
+    return pix;
+}
+
+void TestWidget::drawShadow(QPainter *p)
+{
+    if(FrameWidth == 0){
+        return;
+    }
+    p->save();
+    QColor color(100, 100, 100, 30);
+    for (int i = 0; i < FrameWidth; i++)
+    {
+        color.setAlpha(120 - qSqrt(i) * 40);
+        p->setPen(color);
+        p->drawRoundedRect(FrameWidth - i,
+                                FrameWidth - i,
+                                width() - (FrameWidth - i) * 2,
+                                height() - (FrameWidth - i) * 2,
+                                i,
+                                i);
+    }
+    p->restore();
+}
+
+void TestWidget::drawButtons(QPainter *p)
+{
+    p->save();
+    QRectF rectMini = btnRect(CaptionButtons::Button_Mini);
+    QRectF rectMax = btnRect(CaptionButtons::Button_NormalMax);
+    QRectF rectClose = btnRect(CaptionButtons::Button_Close);
+    if(min_button_hover_){
+        QColor color(0xB9B9B9);
+        color.setAlphaF(0.7);
+        p->fillRect(rectMini, color);
+    }
+    if(max_button_hover_){
+        QColor color(0xB9B9B9);
+        color.setAlphaF(0.7);
+        p->fillRect(rectMax, color);
+    }
+    if(close_button_hover_){
+        p->fillRect(rectClose, QColor(0xE81123));
+    }
+    QPixmap pix = btnPixmap(CaptionButtons::Button_Mini);
+    p->drawPixmap(QPointF(rectMini.x() + (rectMini.width() - pix.width()) / 2,
+                         rectMini.y() + (rectMini.height() - pix.height()) / 2),
+                 pix);
+    pix = btnPixmap(CaptionButtons::Button_NormalMax);
+    p->drawPixmap(QPointF(rectMax.x() + (rectMax.width() - pix.width()) / 2,
+                         rectMax.y() + (rectMax.height() - pix.height()) / 2),
+                 pix);
+    pix = btnPixmap(CaptionButtons::Button_Close);
+    p->drawPixmap(QPointF(rectClose.x() + (rectClose.width() - pix.width()) / 2,
+                         rectClose.y() + (rectClose.height() - pix.height()) / 2),
+                 pix);
+
+    p->restore();
+}
+
+void TestWidget::clearButtonHover()
+{
+    min_button_hover_ = false;
+    max_button_hover_ = false;
+    close_button_hover_ = false;
+    update();
 }
 
--- tests/testwidget.h
+++ tests/testwidget.h
@@ -13,38 +13,69 @@ class TestWidget : public QWidget
 
 public:
     explicit TestWidget(QWidget *parent = nullptr);
-    ~TestWidget();
-
+    ~TestWidget();    
 protected:
     void paintEvent(QPaintEvent *event) override;
     void mousePressEvent(QMouseEvent *event) override;
     void mouseMoveEvent(QMouseEvent *event) override;
     void mouseReleaseEvent(QMouseEvent *event) override;
     void leaveEvent(QEvent *event) override;
+    void changeEvent(QEvent *event) override;
 private:
     Ui::TestWidget *ui;
-    const int FrameWidth = 10;
+    int FrameWidth = 10;
     const int CaptionHeight = 30;
 
-    enum class TenRegion{
-        Left = 1,
-        TopLeft,
-        Top,
-        TitleBar,
-        TopRight,
-        Right,
-        BottomRight,
-        Bottom,
-        BottomLeft,
-        Middle
+    enum class Region{
+        HT_Error = -2,
+        HT_Left,
+        HT_TopLeft,
+        HT_Top,
+        HT_Caption,
+        HT_MinButton,
+        HT_MaxButton,
+        HT_CloseButton,
+        HT_TopRight,
+        HT_Right,
+        HT_BottomRight,
+        HT_Bottom,
+        HT_BottomLeft,
+        HT_Client
     };
 
     bool mouse_left_btn_pressed_ = false;
     QPoint mouse_left_btn_press_pos_;
-    TenRegion region_;
+    Region region_;
 
     // Determine the area and change the shape during the mouse movement
-    void judgeRegionChangeCursor(const QPoint &posInThis);
+    void judgeRegion(const QPoint &windowPos);
+
+    ///
+    /// \brief The CaptionButtons enum
+    ///
+    enum class CaptionButtons{
+        Button_None = -1,
+        Button_Mini,
+        Button_NormalMax,
+        Button_Close,
+    };
+
+    bool min_button_hover_ = false,
+    max_button_hover_ = false,
+    close_button_hover_ = false;
+
+    bool min_button_press_ = false,
+    max_buttton_press_ = false,
+    close_button_press_ = false;
+
+    // Get rectangle of caption buttons
+    QRectF btnRect(CaptionButtons button);
+    QPixmap btnPixmap(CaptionButtons button);
+
+    void drawShadow(QPainter *p);
+    void drawButtons(QPainter *p);
+
+    void clearButtonHover();
 
 };
 
